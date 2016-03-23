cf847e6a110e22ebd83be18faf72a01681509781
Jack Baker <bakerjw@stanford.edu>
2016-03-24 11:24:46 +1300

    Minor clean-up of new scale factor function

--- compute_scale_factor.m
+++ compute_scale_factor.m
@@ -2,34 +2,28 @@ function [scaleFac, minDev] = compute_scale_factor(sampleBig,sampleSmall,meanReq
 % Compute scale factors for a set of candidate ground motions, when using 
 % an Unconditional Spectrum target 
 
-% Determine size of sampleSmall for standard deviation calculations
+
+nGM = size(sampleSmall,1) + 1; % number of ground motions
 scales = 0.1:0.1:maxScale;
 scaleFac = zeros(size(sampleBig,1),1);
-[nGM,~] = size(sampleSmall);
-newGM = nGM+1;
 devTotal = zeros(length(scales),1);
 minDev = zeros(length(scales),1);
 
 for i = 1:size(sampleBig,1)
     for j=1:length(scales)
         
-        sampleSmallNew = [sampleSmall;sampleBig(i,:)+log(scales(j))];
+        sampleSmallNew = [sampleSmall;sampleBig(i,:)+log(scales(j))]; % candidate with ground motion i and scale factor j
         
         % Compute deviations from target
-        avg = sum(sampleSmallNew)./newGM;
+        avg = sum(sampleSmallNew)./nGM;
         devMean = avg - meanReq;
-        devSig = sqrt((1/(newGM-1))*sum((sampleSmallNew-repmat(avg,newGM,1)).^2))-sigma;
+        devSig = sqrt((1/(nGM-1))*sum((sampleSmallNew-repmat(avg,nGM,1)).^2))-sigma;
         devTotal(j) = weights(1) * sum(devMean.^2) + weights(2) * sum(devSig.^2);
         
     end
     
     [minDev(i), minID] = min(devTotal);
-    
-    if minDev(i) == 100000;
-        scaleFac(i) = -99;
-    else
-        scaleFac(i) = scales(minID);
-    end
+    scaleFac(i) = scales(minID);
 
 end
 
