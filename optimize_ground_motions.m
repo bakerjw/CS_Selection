7208bf88496afb779d04da42bb83604abd8bb358
Jack Baker <bakerjw@stanford.edu>
2016-03-23 15:47:33 +1300

    Modify the optimization function to move code into a function and move code outside of the loops

--- optimize_ground_motions.m
+++ optimize_ground_motions.m
@@ -44,84 +44,58 @@ function [ IMs ] = optimize_ground_motions( selectionParams, targetSa, IMs )
 sampleSmall = IMs.sampleSmall;
 
 display('Please wait...This algorithm takes a few minutes depending on the number of records to be selected');
-if selectionParams.cond == 0
+if selectionParams.cond == 0 & selectionParams.isScaled
     display('The algorithm is slower when scaling is used');
 end
 if selectionParams.optType == 1
     display('The algorithm is slower when optimizing with the KS-test Dn statistic');
 end
 
-% if optimizing with KS-test statistic, first calculate the emperical CDF
-% values (which are the same at each period)
-if selectionParams.optType == 1
-    emp_cdf = linspace(0,1,selectionParams.nGM+1);
-end
 
-% Initialize scale factor vector for each iteration and a scale factor
-% vector for the final selected records 
-scaleFac = ones(selectionParams.nBig,1);
-IMs.scaleFac = ones(selectionParams.nGM,1);
+% Initialize scale factor vectors if possible
+
+if selectionParams.isScaled == 0 % no scaling so set scale factors = 1
+    scaleFac = ones(selectionParams.nBig,1);
+    IMs.scaleFac = ones(selectionParams.nGM,1);
+elseif selectionParams.isScaled && selectionParams.cond % Sa(Tcond) scaling
+    scaleFac = exp(selectionParams.lnSa1)./exp(IMs.sampleBig(:,selectionParams.indT1));
+    % get indices of ground motions with allowable scale factors, for further consideration
+    idxAllow = find(scaleFac < selectionParams.maxScale);
+end
 
 hw = waitbar(0,'Optimizing ground motion selection');
 
 for k=1:selectionParams.nLoop % Number of passes
-    for i=1:selectionParams.nGM % Selects nGM ground motions
-        waitbar(((k-1)*selectionParams.nGM + i-1)/(selectionParams.nLoop*selectionParams.nGM)); % update percentage completion 
+    for i=1:selectionParams.nGM % consider replacing each ground motion in the selected set
 
-        devTotal = zeros(selectionParams.nBig,1); % initialize measure of deviation for each selected ground motion
         sampleSmall(i,:) = []; % remove initially selected record to be replaced
         IMs.recID(i,:) = []; 
         
-        if selectionParams.isScaled == 1
-            if selectionParams.cond == 1
-                scaleFac = exp(selectionParams.lnSa1)./exp(IMs.sampleBig(:,selectionParams.indT1));
-            elseif selectionParams.cond == 0
-                [scaleFac, devTotal] = bestScaleFactor(IMs.sampleBig, sampleSmall, targetSa.meanReq, targetSa.stdevs, selectionParams.weights, selectionParams.maxScale);
-            end
+        % scaling with unconditional selection, compute scale factors
+        if selectionParams.isScaled && selectionParams.cond == 0
+            scaleFac = compute_scale_factor(IMs.sampleBig, sampleSmall, targetSa.meanReq, targetSa.stdevs, selectionParams.weights, selectionParams.maxScale);
+            % get indices of ground motions with allowable scale factors, for further consideration
+            idxAllow = find(scaleFac < selectionParams.maxScale);
         end
         
         % Try to add a new spectrum to the subset list
+        devTotal = 1000000 * ones(selectionParams.nBig,1); % initialize to large errors, and recompute for allowable records
         for j = 1:selectionParams.nBig
-            sampleSmall = [sampleSmall;IMs.sampleBig(j,:)+log(scaleFac(j))];
-            % Calculate the candidate's deviation and store in devTotal 
-            if selectionParams.optType == 0
-                if selectionParams.cond == 1 || (selectionParams.cond == 0 && selectionParams.isScaled == 0)
-                    % Compute deviations from target
-                    devMean = mean(sampleSmall) - targetSa.meanReq;
-                    devSig = std(sampleSmall) - targetSa.stdevs;
-                    devTotal(j) = selectionParams.weights(1) * sum(devMean.^2) + selectionParams.weights(2) * sum(devSig.^2);
-                end
-                % Penalize bad spectra (set penalty to zero if this is not required)
-                if selectionParams.penalty ~= 0
-                    for m=1:size(sampleSmall,1)
-                        devTotal(j) = devTotal(j) + sum(abs(exp(sampleSmall(m,:))>exp(targetSa.meanReq+3*targetSa.stdevs'))) * selectionParams.penalty;
-                    end
-                end
-                
-            elseif selectionParams.optType == 1   
-                sortedlnSa = [min(sampleSmall); sort(sampleSmall)];
-                norm_cdf = normcdf(sortedlnSa,repmat(targetSa.meanReq,selectionParams.nGM+1,1),repmat(targetSa.stdevs,selectionParams.nGM+1,1));
-                Dn = max(abs(repmat(emp_cdf',1,length(selectionParams.TgtPer)) - norm_cdf));
-                devTotal(j) = sum(Dn);   
+            
+            if ~any(IMs.recID == j) % if this candidate is not already in the set 
+                testSpectra = [sampleSmall; IMs.sampleBig(j,:)+log(scaleFac(j))]; % add candidate to set
+                devTotal(j) = compute_spectrum_error(selectionParams, targetSa, testSpectra);
             end
             
-            % Scale factor should not exceed the maximum
-            if (scaleFac(j) > selectionParams.maxScale)
-                devTotal(j) = 1000000;
-            end
-            
-            % Record should not be repeated
-            if (any(IMs.recID == j)) %%%%%%%%%%%%% this is checking the wrong set of records--correct this
-                devTotal(j) = 1000000;
-            end
-            sampleSmall = sampleSmall(1:end-1,:);
         end
         
         [~ , minID] = min(devTotal);
         % Add new element in the right slot
         IMs.scaleFac(i) = scaleFac(minID);
-        sampleSmall = [sampleSmall(1:i-1,:);IMs.sampleBig(minID,:)+log(scaleFac(minID));sampleSmall(i:end,:)];
-        IMs.recID = [IMs.recID(1:i-1);minID;IMs.recID(i:end)];
+        IMs.recID = [IMs.recID(1:i-1); minID; IMs.recID(i:end)];
+        sampleSmall = [sampleSmall(1:i-1,:); IMs.sampleBig(minID,:)+log(scaleFac(minID)); sampleSmall(i:end,:)];
+        
+        waitbar(((k-1)*selectionParams.nGM + i)/(selectionParams.nLoop*selectionParams.nGM)); % update waitbar
     end
     
     % Can the optimization be stopped after this loop based on the user
@@ -135,15 +109,12 @@ for k=1:selectionParams.nLoop % Number of passes
         fprintf('Max (across periods) error in median = %3.1f percent \n', meanErr); 
         fprintf('Max (across periods) error in standard deviation = %3.1f percent \n \n', stdErr);
         
-        % If error is now within the tolerance, break out of the
-        % optimization
+        % If error is within the tolerance, break out of the optimization
         if meanErr < selectionParams.tol && stdErr < selectionParams.tol
             display('The percent errors between chosen and target spectra are now within the required tolerances.');
             break;
         end
-    end
-    
-    fprintf('End of loop %1.0f of %1.0f \n', k, selectionParams.nLoop) 
+    end    
 end
 
 close(hw); % close waitbar
