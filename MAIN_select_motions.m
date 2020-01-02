d71445409cc0a30258168f26a57f7c14bace1f32
N Simon Kwong <nealsimonkwong@berkeley.edu>
2020-01-02 14:27:46 -0700

    added user inputs for including V comp

--- MAIN_select_motions.m
+++ MAIN_select_motions.m
@@ -46,13 +46,25 @@
 %           .penalty    : >0 to penalize selected spectra more than 
 %                         3 sigma from the target at any period, 
 %                         =0 otherwise.
-%          . weights    : [Weights for error in mean, standard deviation 
+%           .weights    : [Weights for error in mean, standard deviation 
 %                         and skewness] e.g., [1.0,2.0 0.3] 
 %           .nLoop      : Number of loops of optimization to perform.
 %           .nBig       : The number of spectra that will be searched
 %           .indTcond   : Index of Tcond, the conditioning period
 %           .recID      : Vector of index values for the selected
 %                         spectra
+%           .matchV     : =1 to include vertical (V) components of ground
+%                         motion in the selection process
+%           .TminV      : Shortest vibration period of interest for V
+%                         component (applies only when matchV=1)
+%           .TmaxV      : Longest vibration period of interest for V
+%                         component (applies only when matchV=1)
+%           .weightV    : Weight specifying importance of V component
+%                         relative to H components (applies only when
+%                         matchV=1)
+%           .sepScaleV  : =1 to compute separate scale factor for V
+%                         components via corresponding target spectrum
+%                         (applies only when matchV=1)
 % 
 % rup                   :  A structure with parameters that specify the rupture scenario
 %                          for the purpose of evaluating a GMPE. Here we
@@ -116,7 +128,7 @@ selectionParams.nGM        = 30;  % number of ground motions to be selected
 selectionParams.Tcond      = 1.5; % Period at which spectra should be scaled and matched 
 selectionParams.Tmin       = 0.1; % smallest spectral period of interest
 selectionParams.Tmax       = 10;  % largest spectral period of interest
-
+selectionParams.TgtPer = logspace(log10(selectionParams.Tmin),log10(selectionParams.Tmax),30); % compute an array of periods between Tmin and Tmax
 selectionParams.SaTcond    = 0.3;   % (optional) target Sa(Tcond) to use when 
                                   % computing a conditional spectrum 
                                   % if a value is provided here, rup.eps_bar 
@@ -124,6 +136,14 @@ selectionParams.SaTcond    = 0.3;   % (optional) target Sa(Tcond) to use when
                                   % get_target_spectrum.m. If this =[], the
                                   % rup.eps_bar value specified below will
                                   % be used
+
+% Parameters related to (optional) selection vertical spectra
+selectionParams.matchV          = 1; % =1 to do selection and scaling while matching a vertical spectrum, =0 to not
+selectionParams.TminV           = 0.01; % smallest vertical spectral period of interest
+selectionParams.TmaxV           = 10;  % largest vertical spectral period of interest
+selectionParams.weightV         = 0.5;  % weight on vertical spectral match versus horizontal
+selectionParams.sepScaleV       = 1;  % =1 to scale vertical components separately from horizontal, =0 to have same scale factor for each
+selectionParams.TgtPerV = logspace(log10(selectionParams.TminV),log10(selectionParams.TmaxV),20); % compute an array of periods between Tmin and Tmax
 
 % other parameters to scale motions and evaluate selections 
 selectionParams.isScaled   = 1;       
@@ -151,7 +171,22 @@ rup.Fault_Type  = 1;        % =0 for unspecified fault
                             % =1 for strike-slip fault
                             % =2 for normal fault
                             % =3 for reverse fault
-                        
+
+% Additional seismological parameters as inputs to GMPE by Bozorgnia and Campbell 2016 for V component                        
+rup.Rrup        = 11;       % closest distance to rupture plane (km)
+rup.Rx          = 11;       % horizontal distance to vertical surface projection of the top edge of rupture plane, measured perpendicular to the strike (km)       
+rup.W           = 15;       % down-dip rupture width (km)  
+rup.Ztor        = 0;        % depth to top of rupture (km)  
+rup.Zbot        = 15;       % depth to bottom of seismogenic crust (km)  
+rup.dip         = 90;       % fault dip angle (deg)  
+rup.lambda      = 0;        % rake angle (deg)  
+rup.Fhw         = 0;        % flag for hanging wall 
+rup.Z2p5        = 1;        % depth to Vs=2.5 km/sec (km)  
+rup.Zhyp        = 10;       % hypocentral depth of earthquake, measured from sea level (km)                              
+rup.FRV         = 0;        % flag for reverse and reverse-oblique faulting
+rup.FNM         = 0;        % flag for normal and normal-oblique faulting
+rup.Sj          = 0;        % flag for regional site effects; =1 for Japan sites and =0 otherwise
+
 % Ground motion properties to require when selecting from the database. 
 allowedRecs.Vs30 = [-Inf Inf];     % upper and lower bound of allowable Vs30 values 
 allowedRecs.Mag  = [ 6 7 ];     % upper and lower bound of allowable magnitude values
@@ -172,9 +207,6 @@ outputFile  = 'Output_File.dat'; % File name of the summary output file
 % User inputs end here
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Load the specified ground motion database and screen for suitable motions
-
-selectionParams.TgtPer = logspace(log10(selectionParams.Tmin),log10(selectionParams.Tmax),30); % compute an array of periods between Tmin and Tmax
-
 % Load and screen the database
 [SaKnown, selectionParams, indPer, knownPer, metadata] = screen_database(selectionParams, allowedRecs );
 
