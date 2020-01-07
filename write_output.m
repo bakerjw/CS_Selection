c64f90ecc3b4fb67ddf8db68c3cafd45e35152bc
N Simon Kwong <nealsimonkwong@berkeley.edu>
2020-01-07 12:13:31 -0700

    revised write_output function to include V compo

--- write_output.m
+++ write_output.m
@@ -14,16 +14,23 @@ fin = fopen([outputDir '/' outputFile],'w');
 fprintf(fin, '%s \n \n', metadata.getTimeSeries{1}, metadata.getTimeSeries{2}, metadata.getTimeSeries{3});
 if size(metadata.Filename,2) == 1 % only one ground motion component
     fprintf(fin,'%s \t %s \t %s \t %s \t %s \t %s \n','Record Number','Record Sequence Number','Scale Factor','Component Number','File Name','URL');
-else % two components
+elseif size(metadata.Filename,2) == 2 % two components
     fprintf(fin,'%s \t %s \t %s \t %s \t %s \t %s \t %s \n','Record Number','Record Sequence Number','Scale Factor','File Name Dir. 1','File Name Dir. 2', 'URL 1', 'URL 2');
+else
+    fprintf(fin,'%s \t %s \t %s \t %s \t %s \t %s \t %s \t %s \t %s \t %s \n',...
+        'Record Number','Record Sequence Number','Scale Factor (H)','Scale Factor (V)','File Name Dir. 1','File Name Dir. 2','File Name Dir. 3','URL 1','URL 2','URL 3');
 end
 
 % print record data
 for i = 1 : length(recIdx)
     if size(metadata.Filename,2) == 1 % only one ground motion component
         fprintf(fin,'%d \t %d \t %6.2f \t %d \t %s \t %s \n',i,metadata.recNum(recIdx(i)),IMs.scaleFac(i), metadata.compNum(recIdx(i)), char(metadata.Filename{recIdx(i)}),[char(metadata.dirLocation{recIdx(i)}) char(metadata.Filename{recIdx(i)})]); % Print relevant outputs
-    else 
+    elseif size(metadata.Filename,2) == 2 % two components
         fprintf(fin,'%d \t %d \t %6.2f \t %s \t %s \t %s \t %s \n',i,recIdx(i),IMs.scaleFac(i),char(metadata.Filename{recIdx(i),1}),char(metadata.Filename{recIdx(i),2}),[char(metadata.dirLocation{recIdx(i)}) char(metadata.Filename{recIdx(i),2})],[char(metadata.dirLocation{recIdx(i)}) char(metadata.Filename{recIdx(i),2})]);
+    else
+        fprintf(fin,'%d \t %d \t %6.2f \t %6.2f \t %s \t %s \t %s \t %s \t %s \t %s \n',...
+            i,recIdx(i),IMs.scaleFac(i),IMs.scaleFacV(i),char(metadata.Filename{recIdx(i),1}),char(metadata.Filename{recIdx(i),2}),char(metadata.Filename{recIdx(i),3}),...
+            [char(metadata.dirLocation{recIdx(i)}) char(metadata.Filename{recIdx(i),1})],[char(metadata.dirLocation{recIdx(i)}) char(metadata.Filename{recIdx(i),2})],[char(metadata.dirLocation{recIdx(i)}) char(metadata.Filename{recIdx(i),3})]);
     end
 end
 
