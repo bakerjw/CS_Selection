194b3d457a019c095bf279154dbc4c9822e6a700
Jack Baker <bakerjw@stanford.edu>
2022-08-19 17:29:34 -0700

    Update random number generation command for compatibility with parallel processing

--- simulate_spectra.m
+++ simulate_spectra.m
@@ -3,16 +3,16 @@ function [ simulatedSpectra ] = simulate_spectra( targetSa, selectionParams, see
 
 % Set initial seed for simulation
 if seedValue ~= 0
-    rng(seedValue,'twister'); 
+    rng(seedValue);
 else
-    rng('shuffle','twister');
+    rng('default');
 end
 
 % Generate simulated response spectra with best matches to the target values
 devTotalSim = zeros(nTrials,1);
 for j=1:nTrials
     spectraSample{j} = exp(lhsnorm(targetSa.meanReq,targetSa.covReq,selectionParams.nGM));
-    
+
     % evaluate simulation
     sampleMeanErr = mean(log(spectraSample{j})) - targetSa.meanReq; % how close is the mean of the spectra to the target
     sampleStdErr = std(log(spectraSample{j})) - sqrt(diag(targetSa.covReq))'; % how close is the standard dev. of the spectra to the target
@@ -22,8 +22,7 @@ for j=1:nTrials
                      selectionParams.weights(3) * sum(sampleSkewnessErr.^2); % combine the three error metrics to compute a total error
 end
 
-[~, bestSample] = min(devTotalSim); % find the simulated spectra that best match the targets 
+[~, bestSample] = min(devTotalSim); % find the simulated spectra that best match the targets
 simulatedSpectra = spectraSample{bestSample}; % return the best set of simulations
 
 end
-
